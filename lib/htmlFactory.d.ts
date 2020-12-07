5c22a77612d50e07f5fb39a962d1b05bcd9a16d3
eightfeet <bajiao000000@163.com>
2020-12-07 15:24:28 +0800

    update

--- lib/htmlFactory.d.ts
+++ lib/htmlFactory.d.ts
@@ -1,13 +1,20 @@
 /**
+ * 设置dom的font-size，用于控制子元素的em基准单位，pc端时 font-size = 16px，
+ * 其他按规则计算字体值（屏幕宽度:UI宽度 = 屏幕字体大小:UI字体大小）
+ * @param {HTMLElement} dom
+ * @returns
+ */
+export declare function setEmBase(dom: any, emBase: any): void;
+/**
  * dom Hooks 挂载模板到document的指定目标节点，当目标节点不存在时创建一个并设置基准fontSize
  *
  * @export
  * @param {HTMLElement} dom (Required) html模板
  * @param {String} target (Required) element id
- * @param {String} parentId 父级 id
+ * @param {Number} traget 基准大小
  * @returns
  */
-export declare function createDom(dom: string, target: string, parentId: string, emBase: number): Promise<HTMLElement>;
+export declare function createDom(dom: any, target: any, parentId: any, emBase: any): Promise<unknown>;
 /**
  * 移除指定id的dom及其子节点
  *
