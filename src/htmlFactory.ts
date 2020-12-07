55cdf174698217c21386777b6bc8cd0b6cc35a9e
eightfeet <bajiao000000@163.com>
2020-12-07 15:47:52 +0800

    update

--- src/htmlFactory.ts
+++ src/htmlFactory.ts
@@ -11,33 +11,45 @@ Number.isFinite =
 
 /**
  * 设置dom的font-size，用于控制子元素的em基准单位，pc端时 font-size = 16px，
+ * 如果有emBase传入直接使用emBase为基准字体大小
  * 其他按规则计算字体值（屏幕宽度:UI宽度 = 屏幕字体大小:UI字体大小）
  * @param {HTMLElement} dom
+ * @param {String} parentId
+ * @param {Number} emBase
  * @returns
  */
-export function setEmBase (dom:HTMLElement, emBase:number) {
-	let docEl = window.document.documentElement;
-	let clientWidth = docEl.clientWidth;
-	if (!clientWidth) return;
-	if (emBase) {
-		dom.style.fontSize = `${emBase}px`;
-		return;
-	}
-	if (isPC) {
-		dom.style.fontSize = '16px';
-		return;
+function setEmBase(dom:HTMLElement, parentId:string, emBase:number) {
+    const emBaseValidate = Number.isFinite(emBase);
+    if (emBaseValidate) {
+        dom.style.fontSize = `${emBase}px`;
+        return;
     }
-
+    let docEl = window.document.documentElement;
+    let parEl = window.document.getElementById(parentId);
+    let clientWidth = docEl.clientWidth;
+    let parentWidth = parEl ? parEl.clientWidth : null;
     const baseFont = 31.2;
     const uiWidth = 750;
+    if (parEl) {
+        if (parentWidth >= uiWidth) {
+            dom.style.fontSize = baseFont + 'px';
+        } else {
+            dom.style.fontSize = baseFont * (parentWidth / uiWidth) + 'px';
+        }
+        return;
+    }
+    if (!clientWidth) return;
+    if (isPC) {
+        dom.style.fontSize = '16px';
+        return;
+    }
 
-	if (clientWidth >= uiWidth) {
-		dom.style.fontSize = baseFont + "px";
-	} else {
-		dom.style.fontSize = baseFont * (clientWidth / uiWidth) + "px";
-	}
+    if (clientWidth >= uiWidth) {
+        dom.style.fontSize = baseFont + 'px';
+    } else {
+        dom.style.fontSize = baseFont * (clientWidth / uiWidth) + 'px';
+    }
 }
-
 
 /**
  * dom Hooks 挂载模板到document的指定目标节点，当目标节点不存在时创建一个并设置基准fontSize
@@ -45,35 +57,44 @@ export function setEmBase (dom:HTMLElement, emBase:number) {
  * @export
  * @param {HTMLElement} dom (Required) html模板
  * @param {String} target (Required) element id
- * @param {Number} traget 基准大小
+ * @param {String} parentId 父级 id
  * @returns
  */
-export function createDom(dom: string, target: string, parentId: string, emBase: number) {
-	return new Promise((resolve, reject) => {
-		if (!target || !dom) {
-			reject('function createDom: params "dom" or "target" not found.');
-			return;
-		}
-		const hasTarget = document.getElementById(target);
-		if (hasTarget) {
-			hasTarget.innerHTML = dom;
-		}
-		const div = document.createElement('div');
-		div.setAttribute('id', target);
-		setEmBase(div, emBase);
-		if (parentId) {
-			document.getElementById(parentId)
-				.appendChild(div);
-		} else {
-			document
-				.body
-				.appendChild(div);
-		}
-		const targetDom = document.getElementById(target);
-		targetDom.innerHTML = dom;
-		resolve();
-	});
+
+export function createDom(
+    dom: string,
+    target: string,
+    parentId: string,
+    emBase: number
+) {
+    return new Promise<HTMLElement>((resolve, reject) => {
+        if (!target || !dom) {
+            reject('function createDom: params "dom" or "target" not found.');
+            return;
+        }
+        const hasTarget = document.getElementById(target);
+        if (hasTarget) {
+            hasTarget.innerHTML = dom;
+            return;
+        }
+        const div = document.createElement('div');
+        div.setAttribute('id', target);
+        setEmBase(div, parentId, emBase);
+        const parentIdDom = document.getElementById(parentId);
+        if (parentIdDom) {
+            parentIdDom.appendChild(div);
+            const targetDom = document.getElementById(target);
+            targetDom.innerHTML = dom;
+            resolve(parentIdDom);
+            return;
+        }
+        document.body.appendChild(div);
+        const targetDom = document.getElementById(target);
+        targetDom.innerHTML = dom;
+        resolve(parentIdDom);
+    });
 }
+
 
 
 /**
@@ -107,7 +128,7 @@ export function removeDom(target: string) {
  * @param {*} classes
  * @returns
  */
-export function combineDomByClass(dom: any, classes: { [x: string]: any; }) {
+export function combineDomByClass(dom, classes) {
     return new Promise((resolve, reject) => {
         if (!dom || !classes) {
             reject(
