7db1b68cd4f7aec843c2c687d36558474d65b781
eightfeet <bajiao000000@163.com>
2021-05-07 12:04:07 +0800

    update

--- src/modules/NoticeModal/index.ts
+++ src/modules/NoticeModal/index.ts
@@ -1,159 +1,144 @@
-import Modal, { createInlineStyles as inlineStyle } from "@eightfeet/modal";
-import { NoticeModalStyle } from "./NoticeModal";
-import s from "./NoticeModal.scss";
+import Modal, { createInlineStyles as inlineStyle } from '@eightfeet/modal';
+import { NoticeModalStyle } from './NoticeModal';
+import s from './NoticeModal.scss';
 
 class NoticeModal extends Modal {
-  theme: {
-    contentTop: any;
-    contentBottom: any;
-    submit: any;
-    modalTitle: any;
-    header: any;
-    article: any;
-    footer: any;
-    contentWrap: any;
-  };
-  constructor(
-    {
-      contentTop,
-      contentBottom,
-      submit,
-      header,
-      article,
-      footer,
-      modalTitle,
-      contentWrap,
-      ...other
-    }: NoticeModalStyle,
-    outerFrameId: string
-  ) {
-    super({ style: other, parentId: outerFrameId });
-    this.theme = {
-      contentWrap,
-      contentTop,
-      contentBottom,
-      submit,
-      modalTitle,
-      header,
-      article,
-      footer,
-    };
-  }
+	theme: {
+		contentTop: any;
+		contentBottom: any;
+		submit: any;
+		modalTitle: any;
+		header: any;
+		article: any;
+		footer: any;
+		contentWrap: any;
+	};
+	constructor(
+		{
+			contentTop,
+			contentBottom,
+			submit,
+			header,
+			article,
+			footer,
+			modalTitle,
+			contentWrap,
+			...other
+		}: NoticeModalStyle,
+		outerFrameId: string
+	) {
+		super({ style: other, parentId: outerFrameId });
+		this.theme = {
+			contentWrap,
+			contentTop,
+			contentBottom,
+			submit,
+			modalTitle,
+			header,
+			article,
+			footer
+		};
+	}
 
-  showModal = ({ title, content, footer }, onCancel) => {
-    // state 来自父级
-    const { id } = this.state;
-    // 配置弹窗回调
-    if (onCancel && typeof onCancel === "function") {
-      this.state.onCancel = onCancel;
-    } else {
-      this.state.onCancel = null;
-    }
+	showModal = ({ title, content, footer }, onCancel) => {
+		// state 来自父级
+		const { id } = this.state;
+		// 配置弹窗回调
+		if (onCancel && typeof onCancel === 'function') {
+			this.state.onCancel = onCancel;
+		} else {
+			this.state.onCancel = null;
+		}
 
-    let modalElement = document.getElementById(id);
-    const {
-      contentWrap,
-      contentTop,
-      contentBottom,
-      submit,
-      modalTitle,
-      header,
-      article,
-    } = this.theme;
-    const contentWrapStyle = inlineStyle(contentWrap);
-    const contentTopStyle = inlineStyle(contentTop);
-    const contentBottomStyle = inlineStyle(contentBottom);
-    const modalTitleStyle = inlineStyle(modalTitle);
-    const submitStyle = inlineStyle(submit);
-    const headerStyle = inlineStyle(header);
-    const articleStyle = inlineStyle(article);
-    const footerStyle = inlineStyle(this.theme.footer);
-    const gamedom = `
-			${
-        contentBottomStyle
-          ? `<div class="${s.bottom}" style="${contentBottomStyle}">&nbsp;</div>`
-          : ""
-      }
-			${
-        contentTopStyle
-          ? `<div class="${s.top}" style="${contentTopStyle}">&nbsp;</div>`
-          : ""
-      }
-			<div id="${id}_header" ${headerStyle ? `style="${headerStyle}"` : ""}>
-				${
-          title
-            ? `<div ${
-                modalTitleStyle ? `style="${modalTitleStyle}"` : ""
-              }>${title}</div>`
-            : ""
-        }
+		let modalElement = document.getElementById(id);
+		const {
+			contentWrap,
+			contentTop,
+			contentBottom,
+			submit,
+			modalTitle,
+			header,
+			article
+		} = this.theme;
+		const contentWrapStyle = inlineStyle(contentWrap);
+		const contentTopStyle = inlineStyle(contentTop);
+		const contentBottomStyle = inlineStyle(contentBottom);
+		const modalTitleStyle = inlineStyle(modalTitle);
+		const submitStyle = inlineStyle(submit);
+		const headerStyle = inlineStyle(header);
+		const articleStyle = inlineStyle(article);
+		const footerStyle = inlineStyle(this.theme.footer);
+		const gamedom = `
+			${contentBottomStyle ? `<div class="${s.bottom} ${id}_contentbottom" style="${contentBottomStyle}">&nbsp;</div>` : '' }
+			${contentTopStyle ? `<div class="${s.top} ${id}_contenttop" style="${contentTopStyle}">&nbsp;</div>` : '' }
+			<div id="${id}_header" ${headerStyle ? `style="${headerStyle}"` : ''} class="${id}_header">
+				${title ? `<div ${ modalTitleStyle ? `style="${modalTitleStyle}"` : '' }  class="${id}_modaltitle">${title}</div>` : '' }
 			</div>
-			<div id="${id}_article" ${articleStyle ? `style="${articleStyle}"` : ""}>
-				${content || ""}
-			</div>
-			<div id="${id}_footer" ${footerStyle ? `style="${footerStyle}"` : ""}>
+			<div id="${id}_article" ${articleStyle ? `style="${articleStyle}"` : ''} class="${id}_article"> ${content || ''} </div>
+			<div id="${id}_footer" ${footerStyle ? `style="${footerStyle}"` : ''} class="${id}_footer">
 				${
-          footer
-            ? footer
-            : `<button class="${s.button}" ${
-                submitStyle ? `style="${submitStyle}"` : ""
-              }>确定</button>`
-        }
+					footer
+						? footer
+						: `<button class="${s.button} ${id}_submit" ${
+							submitStyle ? `style="${submitStyle}"` : ''
+						}>确定</button>`
+				}
 			</div>
 		`;
 
-    if (!modalElement) {
-      return this.create(
-        {
-          article: `<div class="${s.mainid} successmodal__content" style="${contentWrapStyle || ''}">
-					${gamedom}
-				</div>`,
-        },
-        true
-      ).then(
-        () =>
-          new Promise((resolve) => {
-            modalElement = document.getElementById(id);
-            const button: HTMLDivElement = modalElement.querySelector(
-              `.${s.button}`
-            );
-            if (!button) {
-              resolve();
-              return;
-            }
-            window.setTimeout(() => {
-              button.onclick = () => {
-                this.hideModal();
-                resolve();
-              };
-            }, 500);
-          })
-      );
-    }
-    const mainElement = modalElement.querySelector(`.${s.mainid}`);
-    mainElement.innerHTML = "";
-    return this.show().then(
-      () =>
-        new Promise((resolve) => {
-          mainElement.innerHTML = gamedom;
-          const button = mainElement.querySelector(`.${s.button}`);
-          if (!button) {
-            resolve();
-            return;
-          }
-          window.setTimeout(() => {
-            (modalElement.querySelector(
-              `.${s.button}`
-            ) as HTMLDivElement).onclick = () => {
-              this.hideModal();
-              resolve();
-            };
-          }, 500);
-        })
-    );
-  };
+		if (!modalElement) {
+			return this.create(
+				{article:
+					`<div class="${s.mainid} successmodal__content ${id}_contentwrap" style="${contentWrapStyle || ''}">
+						${gamedom}
+					</div>`
+				},
+				true
+			).then(
+				() =>
+					new Promise<void>((resolve) => {
+						modalElement = document.getElementById(id);
+						const button: HTMLDivElement = modalElement.querySelector(
+							`.${s.button}`
+						);
+						if (!button) {
+							resolve();
+							return;
+						}
+						window.setTimeout(() => {
+							button.onclick = () => {
+								this.hideModal();
+								resolve();
+							};
+						}, 500);
+					})
+			);
+		}
+		const mainElement = modalElement.querySelector(`.${s.mainid}`);
+		mainElement.innerHTML = '';
+		return this.show().then(
+			() =>
+				new Promise<void>((resolve) => {
+					mainElement.innerHTML = gamedom;
+					const button = mainElement.querySelector(`.${s.button}`);
+					if (!button) {
+						resolve();
+						return;
+					}
+					window.setTimeout(() => {
+						const buttonNode: HTMLDivElement = modalElement.querySelector(
+							`.${s.button}`
+						);
+						buttonNode.onclick = () => {
+							this.hideModal();
+							resolve();
+						};
+					}, 500);
+				})
+		);
+	};
 
-  hideModal = () => this.hide(true);
+	hideModal = () => this.hide(true);
 }
 
 export default NoticeModal;
--- src/modules/ResultModal/index.ts
+++ src/modules/ResultModal/index.ts
@@ -1,194 +1,192 @@
-import Modal, { createInlineStyles as inlineStyle, ModalParameters } from "@eightfeet/modal";
+import Modal, { createInlineStyles as inlineStyle } from "@eightfeet/modal";
 import s from "./result.scss";
-import { ResultModalParameters } from './ResultModal'
+import { ResultModalParameters } from './ResultModal';
 class ResultModal extends Modal {
-  modalTitle: any;
-  submitText: any;
-  submitAddressText: any;
-  onEnsure: any;
-  theme: {
-    modalTitle: any;
-    prizeAlias: any;
-    prizeName: any;
-    awardMsg: any;
-    prizeImg: any;
-    memo: any;
-    contentTop: any;
-    contentBottom: any;
-    submit: any;
-    header: any;
-    article: any;
-    footer: any;
-    contentWrap: any;
-  };
-  constructor({
-    style: {
-      prizeAlias,
-      prizeName,
-      awardMsg,
-      prizeImg,
-      memo,
-      contentTop,
-      contentBottom,
-      submit,
-      header,
-      article,
-      footer,
-      contentWrap,
-      ...other
-    },
-    modalTitle,
-    outerFrameId,
-    onCancel,
-    submitText,
-    onEnsure,
-    submitAddressText,
-    animation
-  }: ResultModalParameters) {
-    super({ style: other, onCancel, parentId: outerFrameId, animation });
+	modalTitle: any;
+	submitText: any;
+	submitAddressText: any;
+	onEnsure: any;
+	theme: {
+		modalTitle: any;
+		prizeAlias: any;
+		prizeName: any;
+		awardMsg: any;
+		prizeImg: any;
+		memo: any;
+		contentTop: any;
+		contentBottom: any;
+		submit: any;
+		header: any;
+		article: any;
+		footer: any;
+		contentWrap: any;
+	};
+	constructor({
+		style: {
+			prizeAlias,
+			prizeName,
+			awardMsg,
+			prizeImg,
+			memo,
+			contentTop,
+			contentBottom,
+			submit,
+			header,
+			article,
+			footer,
+			contentWrap,
+			...other
+		},
+		modalTitle,
+		outerFrameId,
+		onCancel,
+		submitText,
+		onEnsure,
+		submitAddressText,
+		animation
+	}: ResultModalParameters) {
+		super({ style: other, onCancel, parentId: outerFrameId, animation });
 
-    this.modalTitle = modalTitle || null;
-    this.submitText = submitText || "确定";
-    this.submitAddressText = submitAddressText || "去填写地址";
-    this.onEnsure = onEnsure || null;
+		this.modalTitle = modalTitle || null;
+		this.submitText = submitText || "确定";
+		this.submitAddressText = submitAddressText || "去填写地址";
+		this.onEnsure = onEnsure || null;
 
-    this.theme = {
-      contentWrap,
-      modalTitle: other.modalTitle,
-      prizeAlias,
-      prizeName,
-      awardMsg,
-      prizeImg,
-      memo,
-      contentTop,
-      contentBottom,
-      submit,
-      header,
-      article,
-      footer,
-    };
-  }
+		this.theme = {
+			contentWrap,
+			modalTitle: other.modalTitle,
+			prizeAlias,
+			prizeName,
+			awardMsg,
+			prizeImg,
+			memo,
+			contentTop,
+			contentBottom,
+			submit,
+			header,
+			article,
+			footer
+		};
+	}
 
-  showModal = (prize) => {
-    // state 来自父级
-    const { id } = this.state;
-    let modalElement = document.getElementById(id);
-    const {
-      contentWrap,
-      modalTitle,
-      prizeAlias,
-      prizeName,
-      awardMsg,
-      prizeImg,
-      memo,
-      contentTop,
-      contentBottom,
-      submit,
-      header,
-      article,
-      footer,
-    } = this.theme;
-    const contentWrapStyle = inlineStyle(contentWrap);
-    const modalTitleStyle = inlineStyle(modalTitle);
-    const prizeAliasStyle = inlineStyle(prizeAlias);
-    const prizeNameStyle = inlineStyle(prizeName);
-    const awardMsgStyle = inlineStyle(awardMsg);
-    const prizeImgStyle = inlineStyle(prizeImg);
-    const memoStyle = inlineStyle(memo);
-    const contentTopStyle = inlineStyle(contentTop);
-    const contentBottomStyle = inlineStyle(contentBottom);
-    const submitStyle = inlineStyle(submit);
-    const headerStyle = inlineStyle(header);
-    const articleStyle = inlineStyle(article);
-    const footerStyle = inlineStyle(footer);
-    const gamedom = `
+	showModal = (prize) => {
+		// state 来自父级
+		const { id } = this.state;
+		let modalElement = document.getElementById(id);
+		const {
+			contentWrap,
+			modalTitle,
+			prizeAlias,
+			prizeName,
+			awardMsg,
+			prizeImg,
+			memo,
+			contentTop,
+			contentBottom,
+			submit,
+			header,
+			article,
+			footer
+		} = this.theme;
+		const contentWrapStyle = inlineStyle(contentWrap);
+		const modalTitleStyle = inlineStyle(modalTitle);
+		const prizeAliasStyle = inlineStyle(prizeAlias);
+		const prizeNameStyle = inlineStyle(prizeName);
+		const awardMsgStyle = inlineStyle(awardMsg);
+		const prizeImgStyle = inlineStyle(prizeImg);
+		const memoStyle = inlineStyle(memo);
+		const contentTopStyle = inlineStyle(contentTop);
+		const contentBottomStyle = inlineStyle(contentBottom);
+		const submitStyle = inlineStyle(submit);
+		const headerStyle = inlineStyle(header);
+		const articleStyle = inlineStyle(article);
+		const footerStyle = inlineStyle(footer);
+		const gamedom = `
 			${
-        contentBottomStyle
-          ? `<div class="${s.bottom}" style="${contentBottomStyle}">&nbsp;</div>`
-          : ""
-      }
+				contentBottomStyle
+					? `<div class="${s.bottom} ${id}_contentbottom" style="${contentBottomStyle}">&nbsp;</div>`
+					: ""
+			}
 			${
-        contentTopStyle
-          ? `<div class="${s.top}" style="${contentTopStyle}">&nbsp;</div>`
-          : ""
-      }
-			<div ${headerStyle ? `style="${headerStyle}"` : ""}>
+				contentTopStyle
+					? `<div class="${s.top} ${id}_contenttop" style="${contentTopStyle}">&nbsp;</div>`
+					: ""
+			}
+			<div ${headerStyle ? `style="${headerStyle}"` : ""} class="${id}_header">
 				${
-          this.modalTitle
-            ? `<div ${modalTitleStyle ? `style="${modalTitleStyle}"` : ""}>${
-                this.modalTitle
-              }</div>`
-            : ""
-        }
+					this.modalTitle
+						? `<div ${modalTitleStyle ? `style="${modalTitleStyle}"` : ""}  class="${id}_modaltitle">${
+							this.modalTitle
+						}</div>`
+						: ""
+				}
 			</div>
-			<div ${articleStyle ? `style="${articleStyle}"` : ""}>
-				<div ${prizeNameStyle ? `style="${prizeNameStyle}"` : ""}>${
-      prize.prizeName || ""
-    }</div>
+			<div ${articleStyle ? `style="${articleStyle}"` : ""} class="${id}_article">
+				<div ${prizeNameStyle ? `style="${prizeNameStyle}"` : ""}  class="${id}_prizename">${
+					prize.prizeName || ""
+				}</div>
 				${
-          prizeAliasStyle
-            ? `<div style="${prizeAliasStyle}">${prize.prizeAlias || ""}</div>`
-            : ""
-        }
-				<div ${awardMsgStyle ? `style="${awardMsgStyle}"` : ""}>${
-      prize.awardMsg || ""
-    }</div>
+					prizeAliasStyle
+						? `<div style="${prizeAliasStyle}"  class="${id}_prizealias">${prize.prizeAlias || ""}</div>`
+						: ""
+				}
+				<div ${awardMsgStyle ? `style="${awardMsgStyle}"` : ""} class="${id}_awardmsg">${
+					prize.awardMsg || ""
+				}</div>
 				<img ${prizeImgStyle ? `style="${prizeImgStyle}"` : ""} src="${
-      prize.prizeImg
-    }" />
-				<div ${memoStyle ? `style="${memoStyle}"` : ""}>${prize.memo || ""}</div>
+					prize.prizeImg
+				}" class="${id}_prizeimg" />
+				<div ${memoStyle ? `style="${memoStyle}"` : ""} class="${id}_memo">${prize.memo || ""}</div>
 			</div>
-			<div ${footerStyle ? `style="${footerStyle}"` : ""}>
-				<button class="${s.button}" ${submitStyle ? `style="${submitStyle}"` : ""}>${
-      prize.receiveType === 2 ? this.submitAddressText : this.submitText
-    }</button>
+			<div ${footerStyle ? `style="${footerStyle}"` : ""} class="${id}_footer">
+				<button class="${s.button} ${id}_submit" ${submitStyle ? `style="${submitStyle}"` : ""}>${
+					prize.receiveType === 2 ? this.submitAddressText : this.submitText
+				}</button>
 			</div>
 		`;
 
-    if (!modalElement) {
-      return this.create(
-        {
-          article: `<div class="${s.mainid}" style="${contentWrapStyle || ''}">
+		if (!modalElement) {
+			return this.create(
+				{
+					article: `<div class="${s.mainid}  ${id}_contentwrap" style="${contentWrapStyle || ''}">
 					${gamedom}
-				</div>`,
-        },
-        true
-      ).then(
-        () =>
-          new Promise((resolve) => {
-            modalElement = document.getElementById(id);
-            window.setTimeout(() => {
-              (modalElement.querySelector(
-                `.${s.button}`
-              ) as HTMLElement).onclick = () => {
-                this.hideModal();
-                this.onEnsure && this.onEnsure(prize);
-                resolve(prize);
-              };
-            }, 500);
-          })
-      );
-    }
-    const mainElement = modalElement.querySelector(`.${s.mainid}`);
-    mainElement.innerHTML = "";
-    return this.show().then(
-      () =>
-        new Promise((resolve) => {
-          mainElement.innerHTML = gamedom;
-          window.setTimeout(() => {
-            (modalElement.querySelector(
-              `.${s.button}`
-            ) as HTMLElement).onclick = () => {
-              this.hideModal();
-              this.onEnsure && this.onEnsure(prize);
-              resolve(prize);
-            };
-          }, 500);
-        })
-    );
-  };
+				</div>`
+				},
+				true
+			).then(
+				() =>
+					new Promise((resolve) => {
+						modalElement = document.getElementById(id);
+						window.setTimeout(() => {
+							const buttonNode: HTMLElement = modalElement.querySelector(`.${s.button}`);
+							buttonNode.onclick = () => {
+								this.hideModal();
+								this.onEnsure && this.onEnsure(prize);
+								resolve(prize);
+							};
+						}, 500);
+					})
+			);
+		}
+		const mainElement = modalElement.querySelector(`.${s.mainid}`);
+		mainElement.innerHTML = "";
+		return this.show().then(
+			() =>
+				new Promise((resolve) => {
+					mainElement.innerHTML = gamedom;
+					const buttonNode: HTMLElement = modalElement.querySelector(`.${s.button}`);
+					window.setTimeout(() => {
+						buttonNode.onclick = () => {
+							this.hideModal();
+							this.onEnsure && this.onEnsure(prize);
+							resolve(prize);
+						};
+					}, 500);
+				})
+		);
+	};
 
-  hideModal = () => this.hide(true);
+	hideModal = () => this.hide(true);
 }
 
 export default ResultModal;
